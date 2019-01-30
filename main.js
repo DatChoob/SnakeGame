e9f11764034a6a483805488eac9cd32e44b80837
Josh Singh <jashmit04@yahoo.com>
2019-01-29 16:12:40 -0800

    Standard Snake game where snake can go also go through the walls

---
+++ main.js
@@ -0,0 +1,134 @@
+let scale = 10;
+let canvasWidth = 500;
+let canvasHeight = 500;
+var food;
+let snake;
+
+let scoreSpan;
+
+
+function setup() {
+    createCanvas(canvasWidth, canvasHeight);
+    createFood();
+    snake = new Snake();
+    frameRate(15);
+    scoreSpan = createSpan(snake.total).size(100, 100);
+}
+function draw() {
+    background(color(204, 204, 255));
+
+    if (snake.eat(food)) {
+        createFood();
+    }
+
+    drawFood();
+
+    snake.death();
+    snake.update();
+    snake.draw();
+    scoreSpan.html(snake.total);
+}
+
+function keyPressed() {
+    if (keyCode === UP_ARROW) {
+        snake.changeDirection(0, -1);
+    } else if (keyCode === DOWN_ARROW) {
+        snake.changeDirection(0, 1);
+    } else if (keyCode === RIGHT_ARROW) {
+        snake.changeDirection(1, 0);
+    } else if (keyCode === LEFT_ARROW) {
+        snake.changeDirection(-1, 0);
+    }
+}
+
+function drawFood() {
+    fill(255, 0, 100);
+    rect(food.x, food.y, scale, scale);
+}
+function createFood() {
+
+    food = createVector(floor(random(canvasWidth / scale)), floor(random(canvasHeight / scale)));
+    food.mult(scale);
+}
+
+
+
+class Snake {
+
+
+    constructor() {
+        this.position = createVector(floor(random(canvasWidth / scale)), floor(random(canvasHeight / scale)));
+        this.speed = createVector(0, -1);
+        this.tail = []
+        this.total = 0;
+
+    }
+
+
+    changeDirection(x, y) {
+        this.speed = createVector(x, y);
+    }
+
+    eat(foodPosition) {
+        // let distance_between_head_and_food = dist(this.position.x, this.position.y, foodPosition.x, foodPosition.y);
+        let collision = (
+            this.position.x < foodPosition.x + scale &&
+            this.position.x + scale > foodPosition.x &&
+            this.position.y < foodPosition.y + scale &&
+            this.position.y + scale > foodPosition.y);
+
+        if (collision) {
+            this.total++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    death() {
+        for (var i = 0; i < this.tail.length; i++) {
+            var pos = this.tail[i];
+            var d = dist(this.position.x, this.position.y, pos.x, pos.y);
+            if (d < 1) {
+alert("YOU DONE FUCKED UP");
+                this.total = 0;
+                this.tail = [];
+            }
+        }
+    }
+
+    update() {
+        for (var i = 0; i < this.tail.length - 1; i++) {
+            this.tail[i] = this.tail[i + 1];
+        }
+        if (this.total >= 1) {
+            this.tail[this.total - 1] = createVector(this.position.x, this.position.y);
+        }
+
+        this.position.x = this.position.x + (this.speed.x * scale);
+        this.position.y = this.position.y + (this.speed.y * scale);
+        if (this.position.x < - scale/2) {
+            this.position.x = canvasWidth - scale/2;
+        } else if (this.position.x > canvasWidth-scale/2) {
+            this.position.x = 0;
+        } else if (this.position.y < - scale/2) {
+            this.position.y = canvasHeight - scale/2;
+        } else if (this.position.y > canvasHeight-scale/2) {
+            this.position.y = 0;
+        }
+
+        this.position.x = constrain(this.position.x, 0, width - scale);
+        this.position.y = constrain(this.position.y, 0, height - scale);
+    }
+
+
+    draw() {
+        fill(0, 100, 0);
+
+        rect(this.position.x, this.position.y, scale, scale);
+        this.tail.forEach((tailBlock, index) => {
+            rect(tailBlock.x, tailBlock.y, scale, scale);
+        });
+    }
+
+}
